0a50c9608b51aa64a8ee977ca894f7d91efd2d52
Abdelmoez Cherif <abdelmoez.cherif@dg-i.net>
2018-11-01 12:40:07 +0100

    projects view w.i.p.

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,8 +4,8 @@
       <Navigation></Navigation>
       <main class="col-lg-10 offset-lg-2">
         <h1>Projects</h1>
-        <div class="row">
-          <ProjectThumbnail></ProjectThumbnail>
+        <div class="project" v-for="project in projects" :key="project._id">
+          <ProjectThumbnail :project="project"></ProjectThumbnail>
         </div>
       </main>
     </section>
@@ -16,13 +16,32 @@ import Navigation from '../components/Navigation.vue'
 import ProjectThumbnail from '../components/ProjectThumbnail'
 import Header from '../components/Header'
 export default {
+  data: () => {
+    return {
+      pageId: 0,
+      projects: []
+    }
+  },
+  methods: {
+    fetchProjects () {
+      this.$http.get(this.$httpRoutes.GET_PROJECTS, { pageId: this.pageId }).then(({data: { data }}) => {
+        this.projects = this.projects.concat(data || [])
+        this.pageId = this.pageId++
+      })
+    }
+  },
+  mounted () {
+    this.fetchProjects()
+  },
   components: { Navigation, ProjectThumbnail, Header }
 }
 </script>
 
 <style lang="scss" scoped>
 main {
-  position: relative;
-  top: 80px;
+  .project{
+    display: grid;
+    grid-column: repeat(auto-fit, 300px) 
+  } 
 }
 </style>
